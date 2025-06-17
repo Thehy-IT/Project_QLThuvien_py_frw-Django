8c2fedf79c8267e769b3a8e1f01ac1b2eaf07bc4
H.TheHy <hyht9083@ut.edu.vn>
2025-06-17 12:31:48 +0000

    update cấu hình

--- src/urls.py
+++ src/urls.py
@@ -21,13 +21,16 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 
-from src.core import views as core_views
+from src.core import views as core_views # Import các view từ ứng dụng core
 
 urlpatterns = [
-    path("", core_views.index),
-    path("admin/", admin.site.urls),
-    path("__reload__/", include("django_browser_reload.urls")),
+    path("", core_views.index), # Trang chủ của ứng dụng
+    path("admin/", admin.site.urls), # Đường dẫn đến trang quản trị Django
+    path("library/", include("src.library.urls")), # Bao gồm các URL từ ứng dụng library
+    path("__reload__/", include("django_browser_reload.urls")), # Dùng cho việc tải lại trình duyệt tự động khi phát triển
 ]
+
+# Cấu hình phục vụ tệp media và static trong môi trường phát triển
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
