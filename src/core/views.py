7a2e768d8255afad276555c5d5e77fcd2ebf8383
H.TheHy <hyht9083@ut.edu.vn>
2025-06-18 07:43:26 +0000

    version 1

--- src/core/views.py
+++ src/core/views.py
@@ -1,8 +1,4 @@
-"""Tệp này chứa các hàm xem (view functions) cho ứng dụng core"""
 from django.shortcuts import render
 
-def index(request):
-    context = {
-        "title": "Django example",
-    }
-    return render(request, "index.html", context)
+def index_view(request): # <-- Đảm bảo tên hàm là index_view
+    return render(request, 'index.html')
--- src/urls.py
+++ src/urls.py
@@ -1,36 +1,19 @@
-"""
-Tệp này định nghĩa định tuyến URL cho dự án Django.
-URL configuration for QLthuvien project.
-
-The `urlpatterns` list routes URLs to views. For more information please see:
-    https://docs.djangoproject.com/en/5.0/topics/http/urls/
-Examples:
-Function views
-    1. Add an import:  from my_app import views
-    2. Add a URL to urlpatterns:  path('', views.home, name='home')
-Class-based views
-    1. Add an import:  from other_app.views import Home
-    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
-Including another URLconf
-    1. Import the include() function: from django.urls import include, path
-    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
-"""
-
 from django.contrib import admin
 from django.urls import path, include
+from src.core.views import index_view
+
+# Để phục vụ các tệp tĩnh trong môi trường phát triển
 from django.conf import settings
 from django.conf.urls.static import static
 
-from src.core import views as core_views # Import các view từ ứng dụng core
-
 urlpatterns = [
-    path("", core_views.index), # Trang chủ của ứng dụng
-    path("admin/", admin.site.urls), # Đường dẫn đến trang quản trị Django
-    path("library/", include("src.library.urls")), # Bao gồm các URL từ ứng dụng library
-    path("__reload__/", include("django_browser_reload.urls")), # Dùng cho việc tải lại trình duyệt tự động khi phát triển
+    path('admin/', admin.site.urls),
+    path('', index_view, name='index'),
+    path('library/', include('src.library.urls')), # Bao gồm các URL của ứng dụng thư viện
+
+    # Thêm dòng này để django-browser-reload hoạt động
+    path("__reload__/", include("django_browser_reload.urls")),
 ]
 
-# Cấu hình phục vụ tệp media và static trong môi trường phát triển
 if settings.DEBUG:
-    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
