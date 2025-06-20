6c2d95d69ecaf9703cbec62f3201d464172c7e67
H.TheHy <hyht9083@ut.edu.vn>
2025-06-20 03:28:52 +0000

    Định tuyến <đường dẫn> cho ứng dụng QLthuvien

--- src/urls.py
+++ src/urls.py
@@ -1,4 +1,4 @@
-# src/urls.py
+# Tệp định tuyến chính của ứng dụng Django
 from django.contrib import admin
 from django.urls import path, include
 from src.core.views import index_view
@@ -6,13 +6,14 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('', index_view, name='index'),
-    path('library/', include('src.library.urls')),
-    path('accounts/', include('src.users.urls')), # Thêm dòng này để bao gồm các URL của ứng dụng users
+    path('admin/', admin.site.urls),# Đường dẫn đến trang quản trị admin của Django
+    path('', index_view, name='index'),# Trang chủ của ứng dụng, sẽ hiển thị trang index_view
+    path('library/', include('src.library.urls')),# Bao gồm các URL của ứng dụng library
+    path('accounts/', include('src.users.urls')), # bao gồm các URL của ứng dụng users
+    # Đường dẫntự động tải lại trang khi có thay đổi trong mã nguồn
     path("__reload__/", include("django_browser_reload.urls")),
 ]
 
 # Cấu hình để phục vụ static files trong môi trường development
-if settings.DEBUG:
+if settings.DEBUG:# Kiểm tra xem ứng dụng có đang chạy trong chế độ DEBUG hay không
     urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
